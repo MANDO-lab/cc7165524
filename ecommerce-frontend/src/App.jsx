c8f61cba112a2ee18786b7a64b14af465f60e911
MANDO-lab <sachinyes24@gmail.com>
2025-06-13 08:50:59 +0530

    initial commit

---
+++ ecommerce-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='min-h-screen bg-grey-100 p-6'>
+      <h1 className='text-x1 font-bold mb-4 text-center'>E COMMERCE</h1>
+    </div>
+  )
+}
+
+export default App
